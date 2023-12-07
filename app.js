3ce751a6444a42826b050bdfe7f336ef1bbfc0cd
Sumyabazar <bazarbazar821@gmail.com>
2023-12-07 09:58:17 +0800

    created a new css file and did some organize

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-window.document.querySelector(".eb-1 p").addEventListener("click",function(){
-    var submainDom = window.document.querySelector(".eb-1");
-    submainDom.style.width ="840px";
-    // document.getElementById("humuunleg"). = "25px";
-    window.document.getElementById("p1").innerHTML = "New text";
-    
+window.document.querySelector(".eb-1 p").addEventListener("click", function () {
+  var submainDom = window.document.querySelector(".eb-1");
+  submainDom.style.width = "900px";
+  //   submainDom.style.height = "1500px";
+  // document.getElementById("humuunleg"). = "25px";
+  window.document.getElementById("p1").innerHTML = "New text";
 });
-window.document.querySelector(".eb-1 i").addEventListener("click",function(){
-    var submainDom = document.querySelector(".eb-1");
-    submainDom.style.width ="350px";
+window.document.querySelector(".eb-1 i").addEventListener("click", function () {
+  var submainDom = document.querySelector(".eb-1");
+  submainDom.style.width = "350px";
+  window.document.getElementById("p1").innerHTML = "";
 });
-window.document.querySelector(".eb-2 p").addEventListener("click",function(){
-    // alert("amer");
-    var submainDom = document.querySelector(".eb-2");
-    submainDom.style.width ="840px";
+window.document.querySelector(".eb-2 p").addEventListener("click", function () {
+  // alert("amer");
+  var submainDom = document.querySelector(".eb-2");
+  submainDom.style.width = "840px";
 });
-window.document.querySelector(".eb-2 i").addEventListener("click",function(){
-    var submainDom = document.querySelector(".eb-2");
-    submainDom.style.width = "350px";
-    // alert("sdfsdf");
+window.document.querySelector(".eb-2 i").addEventListener("click", function () {
+  var submainDom = document.querySelector(".eb-2");
+  submainDom.style.width = "350px";
+  // alert("sdfsdf");
 });
-window.document.querySelector(".eb-3 p").addEventListener("click",function(){
-    // alert("amer");
-    var submainDom = document.querySelector(".eb-3");
-    submainDom.style.width ="840px";
+window.document.querySelector(".eb-3 p").addEventListener("click", function () {
+  // alert("amer");
+  var submainDom = document.querySelector(".eb-3");
+  submainDom.style.width = "840px";
 });
-window.document.querySelector(".eb-3 i").addEventListener("click",function(){
-    var ebDom = document.querySelector(".eb-3");
-    ebDom.style.width ="350px";
-    // alert("sdfsdf");
+window.document.querySelector(".eb-3 i").addEventListener("click", function () {
+  var ebDom = document.querySelector(".eb-3");
+  ebDom.style.width = "350px";
+  // alert("sdfsdf");
 });
