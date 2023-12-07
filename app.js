62e5ceb5eade10592d09e1ee39149ce76d850ba7
Sumyabazar <bazarbazar821@gmail.com>
2023-12-07 13:42:54 +0800

    main three divs have been got fully responsive

--- app.js
+++ app.js
@@ -1,32 +1,67 @@
 window.document.querySelector(".eb-1 p").addEventListener("click", function () {
   var submainDom = window.document.querySelector(".eb-1");
-  submainDom.style.width = "900px";
-  //   submainDom.style.height = "1500px";
-  // document.getElementById("humuunleg"). = "25px";
-  window.document.getElementById("p1").innerHTML = "New text";
+  submainDom.style.width = "1500px";
+  submainDom.style.height = "1000px";
+  document.getElementById("school-1").style.fontSize = "25px";
+  document.getElementById("school-1").style.textAlign = "left";
+  document.getElementById("school-1").style.margin = "15px";
+  //   document.getElementById("school-1").style.display = "inline";
+
+  window.document.getElementById("p1").textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem explicabo ullam molestiae id sapiente, rem aliquam! Quos accusamus in nihil ut dolorum eligendi harum atque tempore numquam minus sed, facere at laboriosam magnam assumenda debitis. Explicabo quam, ad rem nostrum temporibus quidem id, pariatur maxime dolor, repudiandae iure placeat optio veritatis? Est molestiae consequatur laboriosam, pariatur nemo sint reprehenderit, minima quibusdam illo accusantium suscipit deserunt cupiditate aliquid quaerat mollitia praesentium, possimus aspernatur quas. Neque quod ex quam maiores voluptas reprehenderit qui illo quos, error omnis mollitia enim eaque unde ducimus eos aliquid atque eum magni et ea, aliquam ullam pariatur perferendis. Non reiciendis, pariatur deleniti expedita provident qui, est quo ut deserunt commodi quibusdam in vitae earum distinctio odit nemo aliquid. Alias mollitia unde natus molestiae reiciendis cum labore. Ipsam minima pariatur obcaecati corporis adipisci ratione nostrum, ut ipsum doloremque deserunt aut temporibus autem consequuntur dolorem accusantium? Quaerat, asperiores repellat.";
 });
 window.document.querySelector(".eb-1 i").addEventListener("click", function () {
   var submainDom = document.querySelector(".eb-1");
   submainDom.style.width = "350px";
+  submainDom.style.height = "auto";
+  document.getElementById("school-1").style.fontSize = "30px";
+  document.getElementById("school-1").style.textAlign = "center";
+  document.getElementById("school-1").style.marginTop = "25px";
+  document.getElementById("school-1").style = "!important";
   window.document.getElementById("p1").innerHTML = "";
+  // window.document.getElementById("p1").style.display = "none";
 });
 window.document.querySelector(".eb-2 p").addEventListener("click", function () {
   // alert("amer");
   var submainDom = document.querySelector(".eb-2");
-  submainDom.style.width = "840px";
+  submainDom.style.width = "1500px";
+  submainDom.style.height = "1000px";
+  document.getElementById("school-2").style.fontSize = "25px";
+  document.getElementById("school-2").style.textAlign = "left";
+  document.getElementById("school-2").style.margin = "15px";
+  window.document.getElementById("p2").innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem explicabo ullam molestiae id sapiente, rem aliquam! Quos accusamus in nihil ut dolorum eligendi harum atque tempore numquam minus sed, facere at laboriosam magnam assumenda debitis. Explicabo quam, ad rem nostrum temporibus quidem id, pariatur maxime dolor, repudiandae iure placeat optio veritatis? Est molestiae consequatur laboriosam, pariatur nemo sint reprehenderit, minima quibusdam illo accusantium suscipit deserunt cupiditate aliquid quaerat mollitia praesentium, possimus aspernatur quas. Neque quod ex quam maiores voluptas reprehenderit qui illo quos, error omnis mollitia enim eaque unde ducimus eos aliquid atque eum magni et ea, aliquam ullam pariatur perferendis. Non reiciendis, pariatur deleniti expedita provident qui, est quo ut deserunt commodi quibusdam in vitae earum distinctio odit nemo aliquid. Alias mollitia unde natus molestiae reiciendis cum labore. Ipsam minima pariatur obcaecati corporis adipisci ratione nostrum, ut ipsum doloremque deserunt aut temporibus autem consequuntur dolorem accusantium? Quaerat, asperiores repellat.";
 });
 window.document.querySelector(".eb-2 i").addEventListener("click", function () {
   var submainDom = document.querySelector(".eb-2");
   submainDom.style.width = "350px";
+  submainDom.style.height = "auto";
+  document.getElementById("school-2").style.fontSize = "30px";
+  document.getElementById("school-2").style.textAlign = "center";
+  document.getElementById("school-2").style.marginTop = "25px";
+  document.getElementById("school-2").style = "!important";
+  window.document.getElementById("p2").innerHTML = "";
   // alert("sdfsdf");
 });
 window.document.querySelector(".eb-3 p").addEventListener("click", function () {
   // alert("amer");
   var submainDom = document.querySelector(".eb-3");
-  submainDom.style.width = "840px";
+  submainDom.style.width = "1500px";
+  submainDom.style.height = "1000px";
+  document.getElementById("school-3").style.fontSize = "25px";
+  document.getElementById("school-3").style.textAlign = "left";
+  document.getElementById("school-3").style.margin = "15px";
+  window.document.getElementById("p3").innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem explicabo ullam molestiae id sapiente, rem aliquam! Quos accusamus in nihil ut dolorum eligendi harum atque tempore numquam minus sed, facere at laboriosam magnam assumenda debitis. Explicabo quam, ad rem nostrum temporibus quidem id, pariatur maxime dolor, repudiandae iure placeat optio veritatis? Est molestiae consequatur laboriosam, pariatur nemo sint reprehenderit, minima quibusdam illo accusantium suscipit deserunt cupiditate aliquid quaerat mollitia praesentium, possimus aspernatur quas. Neque quod ex quam maiores voluptas reprehenderit qui illo quos, error omnis mollitia enim eaque unde ducimus eos aliquid atque eum magni et ea, aliquam ullam pariatur perferendis. Non reiciendis, pariatur deleniti expedita provident qui, est quo ut deserunt commodi quibusdam in vitae earum distinctio odit nemo aliquid. Alias mollitia unde natus molestiae reiciendis cum labore. Ipsam minima pariatur obcaecati corporis adipisci ratione nostrum, ut ipsum doloremque deserunt aut temporibus autem consequuntur dolorem accusantium? Quaerat, asperiores repellat.";
 });
 window.document.querySelector(".eb-3 i").addEventListener("click", function () {
+  //   alert("afs");
   var ebDom = document.querySelector(".eb-3");
   ebDom.style.width = "350px";
-  // alert("sdfsdf");
+  ebDom.style.height = "auto";
+  document.getElementById("school-3").style.fontSize = "30px";
+  document.getElementById("school-3").style.textAlign = "center";
+  document.getElementById("school-3").style.marginTop = "25px";
+  document.getElementById("school-3").style = "!important";
+  window.document.getElementById("p3").innerHTML = "";
 });
