0a696d93dc8efaea2bcbd6610f21a8336009b2e8
Sumyabazar <bazarbazar821@gmail.com>
2023-12-13 18:07:04 +0800

    contact cooperation plan holbow

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.document.querySelector(".eb-1 p").addEventListener("click", function () {
+  document.getElementById("exit-button-1").style.visibility = "visible";
   var submainDom = window.document.querySelector(".eb-1");
   submainDom.style.width = "1500px";
   submainDom.style.height = "1000px";
@@ -8,9 +9,10 @@ window.document.querySelector(".eb-1 p").addEventListener("click", function () {
   //   document.getElementById("school-1").style.display = "inline";
 
   window.document.getElementById("p1").innerHTML =
-    "<h1>Ажлийн байранд суурилсан сургалтын хөтөлбөрт хамрагдсан хөтөлбөрүүд</h1><br><br><h4><span>&#183;</span>    Эрх зүй</h4>";
+    "<h1>Ажлийн байранд суурилсан сургалтын хөтөлбөрт хамрагдсан хөтөлбөрүүд</h1><br><br><h4><span>&#183;</span>&nbsp&nbspГадаад хэлний орчуулга</h4><h4><span>&#183;</span>&nbsp&nbspСэтгүүл зүй<h4><span>&#183;</span>&nbsp&nbspБагш гадаад хэлний боловсрол/Англи хэл</h4></h4><h4><span>&#183;</span>&nbsp&nbspБагш сургуулийн өмнөх насны боловсрол</h4><h4><span>&#183;</span>&nbsp&nbspОлон улсын харилцаа</h4><h4><span>&#183;</span>&nbsp&nbspАгаарын тээврийн үйлчилгээ/онгоцны үйлчлэгч</h4><h4><span>&#183;</span>&nbsp&nbspСэтгэл судлал</h4><h4><span>&#183;</span>&nbsp&nbspГадаад хэлний орчуулга/Хятад хэл</h4><h4><span>&#183;</span>&nbsp&nbspНийгмийн ажил</h4><h4><span>&#183;</span>&nbsp&nbspБагш, Монгол хэл уран зохиолын боловсрол</h4>";
 });
 window.document.querySelector(".eb-1 i").addEventListener("click", function () {
+  document.getElementById("exit-button-1").style.visibility = "hidden";
   var submainDom = document.querySelector(".eb-1");
   submainDom.style.width = "350px";
   submainDom.style.height = "auto";
@@ -23,6 +25,7 @@ window.document.querySelector(".eb-1 i").addEventListener("click", function () {
 });
 window.document.querySelector(".eb-2 p").addEventListener("click", function () {
   // alert("amer");
+  document.getElementById("exit-button-2").style.visibility = "visible";
   var submainDom = document.querySelector(".eb-2");
   submainDom.style.width = "1500px";
   submainDom.style.height = "1000px";
@@ -30,9 +33,10 @@ window.document.querySelector(".eb-2 p").addEventListener("click", function () {
   document.getElementById("school-2").style.textAlign = "left";
   document.getElementById("school-2").style.margin = "15px";
   window.document.getElementById("p2").innerHTML =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem explicabo ullam molestiae id sapiente, rem aliquam! Quos accusamus in nihil ut dolorum eligendi harum atque tempore numquam minus sed, facere at laboriosam magnam assumenda debitis. Explicabo quam, ad rem nostrum temporibus quidem id, pariatur maxime dolor, repudiandae iure placeat optio veritatis? Est molestiae consequatur laboriosam, pariatur nemo sint reprehenderit, minima quibusdam illo accusantium suscipit deserunt cupiditate aliquid quaerat mollitia praesentium, possimus aspernatur quas. Neque quod ex quam maiores voluptas reprehenderit qui illo quos, error omnis mollitia enim eaque unde ducimus eos aliquid atque eum magni et ea, aliquam ullam pariatur perferendis. Non reiciendis, pariatur deleniti expedita provident qui, est quo ut deserunt commodi quibusdam in vitae earum distinctio odit nemo aliquid. Alias mollitia unde natus molestiae reiciendis cum labore. Ipsam minima pariatur obcaecati corporis adipisci ratione nostrum, ut ipsum doloremque deserunt aut temporibus autem consequuntur dolorem accusantium? Quaerat, asperiores repellat.";
+    "<h1>Ажлийн байранд суурилсан сургалтын хөтөлбөрт хамрагдсан хөтөлбөрүүд</h1><br><br><h4><span>&#183;</span>&nbsp&nbspБизнесийн удирдлага</h4><h4><span>&#183;</span>&nbsp&nbspНягтлан бодох бүртгэл<h4><span>&#183;</span>&nbsp&nbspСанхүү</h4></h4><h4><span>&#183;</span>&nbsp&nbspАялал жуулчлалын менежмент</h4><h4><span>&#183;</span>&nbsp&nbspПрограмм хангамж</h4><h4><span>&#183;</span>&nbsp&nbspБанк</h4>";
 });
 window.document.querySelector(".eb-2 i").addEventListener("click", function () {
+  document.getElementById("exit-button-2").style.visibility = "hidden";
   var submainDom = document.querySelector(".eb-2");
   submainDom.style.width = "350px";
   submainDom.style.height = "auto";
@@ -45,6 +49,7 @@ window.document.querySelector(".eb-2 i").addEventListener("click", function () {
 });
 window.document.querySelector(".eb-3 p").addEventListener("click", function () {
   // alert("amer");
+  document.getElementById("exit-button-3").style.visibility = "visible";
   var submainDom = document.querySelector(".eb-3");
   submainDom.style.width = "1500px";
   submainDom.style.height = "1000px";
@@ -52,10 +57,11 @@ window.document.querySelector(".eb-3 p").addEventListener("click", function () {
   document.getElementById("school-3").style.textAlign = "left";
   document.getElementById("school-3").style.margin = "15px";
   window.document.getElementById("p3").innerHTML =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem explicabo ullam molestiae id sapiente, rem aliquam! Quos accusamus in nihil ut dolorum eligendi harum atque tempore numquam minus sed, facere at laboriosam magnam assumenda debitis. Explicabo quam, ad rem nostrum temporibus quidem id, pariatur maxime dolor, repudiandae iure placeat optio veritatis? Est molestiae consequatur laboriosam, pariatur nemo sint reprehenderit, minima quibusdam illo accusantium suscipit deserunt cupiditate aliquid quaerat mollitia praesentium, possimus aspernatur quas. Neque quod ex quam maiores voluptas reprehenderit qui illo quos, error omnis mollitia enim eaque unde ducimus eos aliquid atque eum magni et ea, aliquam ullam pariatur perferendis. Non reiciendis, pariatur deleniti expedita provident qui, est quo ut deserunt commodi quibusdam in vitae earum distinctio odit nemo aliquid. Alias mollitia unde natus molestiae reiciendis cum labore. Ipsam minima pariatur obcaecati corporis adipisci ratione nostrum, ut ipsum doloremque deserunt aut temporibus autem consequuntur dolorem accusantium? Quaerat, asperiores repellat.";
+    "<h1>Ажлийн байранд суурилсан сургалтын хөтөлбөрт хамрагдсан хөтөлбөрүүд</h1><br><br><h4><span>&#183;</span>&nbsp&nbspЭрх зүй</h4>";
 });
 window.document.querySelector(".eb-3 i").addEventListener("click", function () {
   //   alert("afs");
+  document.getElementById("exit-button-3").style.visibility = "hidden";
   var ebDom = document.querySelector(".eb-3");
   ebDom.style.width = "350px";
   ebDom.style.height = "auto";
