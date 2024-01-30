3a16495206f8ec6bccf1a77361059dcf435c24a3
Sumyabazar00 <bazarbazar821@gmail.com>
2024-01-30 20:27:23 +0800

    exprienceiin grid bolon busad jijig zuilsiin tsegtslej

--- app1.js
+++ app1.js
@@ -1,21 +1,21 @@
 var del1 = window.document.getElementById("delgerengui");
 del1.addEventListener("click", function () {
   del1.style.display = "none";
-  var a = document.getElementById("exp-info");
+  var a = document.getElementById("cont");
   a.style.display = "block";
   var b = document.getElementById("huraangui");
   b.style.display = "block";
 });
 var hur = window.document.getElementById("huraangui");
 hur.addEventListener("click", function () {
-  var a = document.getElementById("exp-info");
+  var a = document.getElementById("cont");
   a.style.display = "none";
   hur.style.display = "none";
   del1.style.display = "block";
 });
 var del = window.document.getElementById("delgerengui1");
 del.addEventListener("click", function () {
-  var a = document.getElementById("exp-info-1");
+  var a = document.getElementById("cont-1");
   a.style.display = "block";
   del.style.display = "none";
   var b = document.getElementById("huraangui1");
@@ -23,7 +23,7 @@ del.addEventListener("click", function () {
 });
 var hur = window.document.getElementById("huraangui1");
 hur.addEventListener("click", function () {
-  var a = document.getElementById("exp-info-1");
+  var a = document.getElementById("cont-1");
   a.style.display = "none";
   hur.style.display = "none";
   del.style.display = "block";
